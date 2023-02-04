a2cec6a27a492e7f7f5defaf28be5abe44ddb462
shikhar13012001 <ishgupta2015@gmail.com>
2023-02-05 04:59:07 +0530

    added

--- backup/index.js
+++ backup/index.js
@@ -12,33 +12,40 @@ const takeBackup = async () => {
   const token = `Bearer ${args[0]}`;
   const projectId = args[1];
 
-  const { data } = await axios.get(
-    `https://${projectId}.api.sanity.io/v2021-06-07/data/export/production`,
-    {
-      headers: {
-        Authorization: token.replace(/\r?\n|\r/g, ""),
-      },
-    }
-  ).catch((err) => {
-    console.error(err);
+  const { data } = await axios
+    .get(
+      `https://${projectId}.api.sanity.io/v2021-06-07/data/export/production`,
+      {
+        headers: {
+          Authorization: token.replace(/\r?\n|\r/g, ""),
+        },
+      }
+    )
+    .catch((err) => {
+      console.error(err);
     });
 
   // console.log(data);
-   fs.writeFileSync("test.json", data);
+  fs.writeFileSync(`${__dirname}/test.json`, data);
   // run npx ndjson-to-json-text  test.json > final.json to convert ndjson to json using child_process
-  const { execSync } = require("child_process");
+  const { exec } = require("child_process");
+  const path = `backups/backup.json`;
+  console.log(path);
+    exec(
+        `cd backup && npx ndjson-to-json test.json > ${path}`,
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            }
+    );
+
   
-  execSync(
-    `npx ndjson-to-json test.json > ${__dirname}/backup/backups/backup.json`,
-    (err, stdout, stderr) => {
-      if (err) {
-        //some err occurred
-        console.error(err);
-      } else {
-        // delete test.json
-        fs.unlinkSync("test.json");
-      }
-    }
-  );
 };
 takeBackup();
